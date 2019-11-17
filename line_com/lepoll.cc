736ae2bc3eab56d291f6501afd344cc5bdf052d5
bfzq <bigbuggiggle@hotmail.com>
2019-11-17 10:04:23 +0800

    epoll

--- line_com/lepoll.cc
+++ line_com/lepoll.cc
@@ -3,10 +3,19 @@
 #include "line_log_tag.h"
 #include <sys/epoll.h>
 #include <sys/errno.h>
-ret_type register_epoll(int socked, uint32_t events, uint size)
+#include <unistd.h>
+
+static uint wait_epoll_num_ = 0;
+static int wait_timeout_ = 0;
+
+ret_type register_epoll(int &epollfd,
+                        const int &socked,
+                        const uint32_t &register_events,
+                        const uint &register_num,
+                        const uint wait_epoll_num,
+                        const int wait_timeout)
 {
-  int epollfd;
-  epollfd = epoll_create(size);
+  epollfd = epoll_create(register_num);
   if (epollfd == -1)
   {
     line_log(ERROR, LINE_LOG_TAG_NET, errno, "Epoll init error.");
@@ -14,11 +23,43 @@ ret_type register_epoll(int socked, uint32_t events, uint size)
   }
   struct epoll_event ev;
   ev.data.fd = socked;
-  ev.events = events;
+  ev.events = register_events;
   int r = epoll_ctl(epollfd, EPOLL_CTL_ADD, socked, &ev);
   if (r == -1)
   {
     line_log(ERROR, LINE_LOG_TAG_NET, errno, "Epoll init error.");
+    return ret_failed;
+  }
+  update_epoll_config(wait_epoll_num, wait_timeout);
+  return ret_successful;
+}
+
+void update_epoll_config(const uint wait_epoll_num,
+                         const int wait_timeout)
+{
+  wait_epoll_num_ = wait_epoll_num;
+  wait_timeout_ = wait_timeout;
+}
+
+ret_type wait_epoll(const int &epollfd,
+                    struct epoll_event *events,
+                    uint &events_num)
+{
+  int r = events_num = epoll_wait(epollfd, events, wait_epoll_num_, wait_timeout_);
+  if (r == -1)
+  {
+    line_log(ERROR, LINE_LOG_TAG_NET, errno, "Wait epoll error.");
+    return ret_failed;
+  }
+  return ret_successful;
+}
+
+ret_type cancel_epoll(const int &epoll)
+{
+  int r = close(epoll);
+  if (r == -1)
+  {
+    line_log(ERROR, LINE_LOG_TAG_NET, errno, "Close epoll error.");
     return ret_failed;
   }
   return ret_successful;
--- line_com/lepoll.h
+++ line_com/lepoll.h
@@ -4,6 +4,16 @@
 #include "ltype.h"
 #include "ret_code.h"
 #include <stdint.h>
-ret_type register_epoll(int socked, uint32_t events, uint size);
-
+ret_type register_epoll(int &epollfd,
+                        const int &socked,
+                        const uint32_t &register_events,
+                        const uint &register_num,
+                        const uint wait_epoll_num = 10,
+                        const int wait_timeout = 10);
+void update_epoll_config(const uint wait_epoll_num = 10,
+                         const int wait_timeout = 10);
+ret_type wait_epoll(const int &epollfd,
+                    struct epoll_event *events,
+                    uint events_num);
+ret_type cancel_epoll(const int &epoll);
 #endif // _LEPOLL_H_
